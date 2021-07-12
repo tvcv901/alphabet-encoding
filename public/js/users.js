b8103cf19c9d48f2a72607b99bafad0ee73cdbc7
Chandra Vamsi <vamsi.tvcv901@gmail.com>
2021-07-12 20:13:34 +0530

    Fixed submit in chat page when clicked
    New socket connection doesn't get established when submit is clicked
    After clicking submit, message box is cleared

--- public/js/users.js
+++ public/js/users.js
@@ -1,2 +1,18 @@
 // connecting client to server
 const socket = io();
+
+const chatForm = document.getElementById('chat-form');
+console.log(chatForm); // works
+
+chatForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	let msg = e.target.elements.msg.value.trim();
+	if (!msg) { return false; }
+	console.log(msg); // works!!!
+	// emit message to server
+
+	// clear the contents
+	e.target.elements.msg.value = '';
+	e.target.elements.msg.focus();
+});
